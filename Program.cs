82120c444c295f131ecb4259533042c25672c277
Paulo Maia <pauloafmaia9@gmail.com>
2023-12-20 00:34:11 -0300

    Alterações

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia(numero: "1.1111-1111", modelo: "1", imei: "11111111", memoria: 128);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone Iphone = new Iphone(numero: "2.2222-2222", modelo: "2", imei: "22222222", memoria: 256);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Instagram");
